93803b1267159dcba53ad7db229087d2d92e1885
anas-derk <anas.derk2023@gmail.com>
2025-02-27 14:04:57 +0300

    Fix

--- src/pages/orders-managment/[orderId]/index.jsx
+++ src/pages/orders-managment/[orderId]/index.jsx
@@ -102,8 +102,8 @@ export default function OrderDetails({ orderIdAsProperty }) {
         catch (err) {
             console.log(err);
             if (err?.response?.status === 401) {
-                // localStorage.removeItem(process.env.adminTokenNameInLocalStorage);
-                // await router.replace("/login");
+                localStorage.removeItem(process.env.adminTokenNameInLocalStorage);
+                await router.replace("/login");
             }
             else {
                 setWaitMsg("");
@@ -220,7 +220,7 @@ export default function OrderDetails({ orderIdAsProperty }) {
                                                     disabled={orderDetails.isDeleted || orderDetails.checkoutStatus !== "Checkout Successfull"}
                                                 />
                                             </td>
-                                             <td>
+                                            <td>
                                                 {orderProduct.unitDiscount}
                                             </td>
                                             <td>
