4d643b4c1558a3e8c21cdca5921d3e5e2a227d22
anas derk <anas.derk@gmail.com>
2025-06-17 16:51:33 +0100

    Continue

--- locals/ar/index.js
+++ locals/ar/index.js
@@ -1,38 +1,38 @@
 import headerTranslations from "../ar/components/header.translation.json";
-import billingTranslations from "../ar/pages/billing.translation.json";
 import homeTranslations from "../ar/pages/home.translation.json";
 import globalTranslations from "../ar/global/global.translation.json";
-import usersTranslations from "../ar/pages/users.translation.json";
-import storesTranslations from "../ar/pages/stores.translation.json";
+import updateAndDeleteUsersTranslations from "../ar/pages/users/update_and_delete_users.translation.json";
+import updateAndDeleteStoresTranslations from "../ar/pages/stores/update_and_delete_stores.translation.json";
 import changeStoreStatusBoxTranslations from "../ar/components/change_store_status_box.translate.json";
 import loginTranslations from "../ar/pages/login.translation.json";
-import addNewAdminTranslations from "../ar/pages/add_new_admin.translation.json";
-import adminsTranslations from "../ar/pages/admins.translations.json";
-import addNewProductTranslations from "../ar/pages/add_new_product.json";
-import updateAndDeleteProductsTranslations from "../ar/pages/update_and_delete_products.json";
-import updateAndDeleteProductGalleryImagesTranslations from "../ar/pages/update_and_delete_product_gallery_images.json";
-import addNewGalleryImagesTranslations from "../ar/pages/add_new_gallery_images.json";
-import updateAndDeleteProductCategoriesTranslations from "../ar/pages/update_and_delete_product_categories.json";
-import addNewCategoryTranslations from "../ar/pages/add_new_category.json";
-import updateAndDeleteCategoriesTranslations from "../ar/pages/update_and_delete_categories.json";
-import updateCategoryParentTranslations from "../ar/pages/update_category_parent.json";
-import addNewBrandTranslations from "../ar/pages/add_new_brand.json";
-import updateAndDeleteBrandsTranslations from "../ar/pages/update_and_delete_brands.json";
-import addNewAdvertimsmentTranslations from "../ar/pages/add_new_advertisment.json";
-import updateAndDeleteAdsTranslations from "../ar/pages/update_and_delete_ads.json";
-import updateAndDeleteOrdersTranslations from "../ar/pages/update_and_delete_orders.json";
+import addNewAdminTranslations from "../ar/pages/admins/add_new_admin.translation.json";
+import updateAndDeleteAdminsTranslations from "../ar/pages//admins/update_and_delete_admins.translations.json";
+import addNewProductTranslations from "../ar/pages//products/add_new_product.translation.translation.json";
+import updateAndDeleteProductsTranslations from "../ar/pages/products/update_and_delete_products.translation.json";
+import addNewGalleryImagesTranslations from "../ar/pages/products/gallery_images/add_new_gallery_images.translation.json";
+import updateAndDeleteProductGalleryImagesTranslations from "../ar/pages/products/gallery_images/update_and_delete_product_gallery_images.translation.json";
+import updateAndDeleteProductCategoriesTranslations from "../ar/pages/products/categories/update_and_delete_product_categories.translation.json";
+import addNewCategoryTranslations from "../ar/pages/categories/add_new_category.translation.translation.json";
+import updateAndDeleteCategoriesTranslations from "../ar/pages/categories/update_and_delete_categories.translation.json";
+import updateCategoryParentTranslations from "../ar/pages/categories/update_category_parent.translation.json";
+import addNewBrandTranslations from "../ar/pages/brands/add_new_brand.translation.json";
+import updateAndDeleteBrandsTranslations from "../ar/pages/brands/update_and_delete_brands.translation.json";
+import addNewAdvertimsmentTranslations from "../ar/pages/ads/add_new_advertisment.translation.json";
+import updateAndDeleteAdsTranslations from "../ar/pages/ads/update_and_delete_ads.translation.json";
+import updateAndDeleteOrdersTranslations from "../ar/pages/orders/update_and_delete_orders.translation.json";
+import billingTranslations from "../ar/pages//orders/billing.translation.json";
 
 export default {
     ...headerTranslations,
     ...billingTranslations,
     ...homeTranslations,
     ...globalTranslations,
-    ...usersTranslations,
-    ...storesTranslations,
+    ...updateAndDeleteUsersTranslations,
+    ...updateAndDeleteStoresTranslations,
     ...changeStoreStatusBoxTranslations,
     ...loginTranslations,
     ...addNewAdminTranslations,
-    ...adminsTranslations,
+    ...updateAndDeleteAdminsTranslations,
     ...addNewProductTranslations,
     ...updateAndDeleteProductsTranslations,
     ...updateAndDeleteProductGalleryImagesTranslations,
