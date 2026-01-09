4446f2fccd23e118e145fea54c06230f8f1b10a7
anas derk <anas.derk@gmail.com>
2026-01-10 00:48:16 +0300

    Test

--- src/pages/orders-managment/[orderId]/index.jsx
+++ src/pages/orders-managment/[orderId]/index.jsx
@@ -192,17 +192,17 @@ export default function OrderDetails({ orderIdAsProperty, ordersType }) {
                                                 />
                                             </td>
                                             {ordersType === "normal" && <td>
-                                                {/* <h6 className="mb-4 fw-bold">{t("Message")}:</h6>
+                                                <h6 className="mb-4 fw-bold">{t("Message")}:</h6>
                                                 <h6 className="mb-4 fw-bold">{orderProduct?.message ?? "-------"}</h6>
-                                                <hr />
+                                                {/* <hr />
                                                 <h6 className="mb-4 fw-bold">{t("Custom Text")}:</h6>
                                                 <h6 className="mb-4 fw-bold">{orderProduct?.extraData?.customText ?? "-------"}</h6>
                                                 <hr />
                                                 <h6 className="mb-4 fw-bold">{t("Additional Notes")}:</h6>
                                                 <h6 className="mb-4 fw-bold">{orderProduct?.extraData?.additionalNotes ?? "-------"}</h6>
                                                 <hr />
-                                                <h6 className="mb-4 fw-bold">{t("Additional Files")}:</h6>
-                                                {orderProduct?.extraData?.additionalFiles?.length > 0 ? <div className="mb-4 fw-bold additional-files">
+                                                <h6 className="mb-4 fw-bold">{t("Additional Files")}:</h6> */}
+                                                {/* {orderProduct?.extraData?.additionalFiles?.length > 0 ? <div className="mb-4 fw-bold additional-files">
                                                     {orderProduct.extraData.additionalFiles.map((file, fileIndex) => (
                                                         <div className="file-download-box mb-3" key={fileIndex}>
                                                             {!waitMsg && !errorMsg && selectedOrderProductIndex !== orderProductIndex && selectedFileIndex !== fileIndex && <button
