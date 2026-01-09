43a315f24b260f7cb632070ef6e3c8c97f65b4c2
anas derk <anas.derk@gmail.com>
2026-01-10 01:01:25 +0300

    Test

--- src/pages/orders-managment/[orderId]/index.jsx
+++ src/pages/orders-managment/[orderId]/index.jsx
@@ -197,9 +197,9 @@ export default function OrderDetails({ orderIdAsProperty, ordersType }) {
                                                 <hr />
                                                 <h6 className="mb-4 fw-bold">{t("Custom Text")}:</h6>
                                                 <h6 className="mb-4 fw-bold">{orderProduct?.extraData?.customText ? orderProduct.extraData.customText : "-------"}</h6>
-                                                <hr />
+                                                {/* <hr />
                                                 <h6 className="mb-4 fw-bold">{t("Additional Notes")}:</h6>
-                                                <h6 className="mb-4 fw-bold">{orderProduct?.extraData?.additionalNotes ? orderProduct.extraData.customText : "-------"}</h6>
+                                                <h6 className="mb-4 fw-bold">{orderProduct?.extraData?.additionalNotes ? orderProduct.extraData.additionalNotes : "-------"}</h6> */}
                                                 <hr />
                                                 <h6 className="mb-4 fw-bold">{t("Additional Files")}:</h6>
                                                 {/* {orderProduct?.extraData?.additionalFiles?.length > 0 ? <div className="mb-4 fw-bold additional-files">
